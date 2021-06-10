c24d4b3a5b139c0f75a0cb757ef5934ebfd61bf5
Cody Robinson <cody.robinson150@gmail.com>
2021-06-10 21:04:11 +0100

    Implemented template driven approach for forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { MediaItemComponent } from './media-item.component';
+import { MediaItemFormComponent } from './media-item-form.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ],
     declarations: [
         AppComponent,
         MediaItemComponent,
         MediaItemListComponent,
+        MediaItemFormComponent,
         FavoriteDirective,
         CategoryListPipe
     ],
